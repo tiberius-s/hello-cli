36866d46f64c45ed763b79b806d5912aab630b73
Tiberius Silivestru <tiberius.silivestru@gmail.com>
2022-07-31 23:00:38 -0500

    adds eslint configuration, inserts shebang at build

--- src/cli.ts
+++ src/cli.ts
@@ -1,20 +1,19 @@
-#!/usr/bin/env node --experimental-modules=node --experimental-specifier-resolution=node
-
 import { Command } from 'commander';
 
 import { helloCommand } from './commands/index.js';
 
 const program = new Command();
-
 program.addCommand(helloCommand());
-
 program.exitOverride();
 
-// This can all be wrapped in an async main function in case any of the command handlers are asynchronous
-try {
-  program.parse(process.argv);
-} catch (err) {
-  // handle the exit your way
-  console.error(`ERR: ${err}`);
-  process.exit(1);
+async function main() {
+  try {
+    await program.parseAsync(process.argv);
+  } catch (err) {
+    // handle the exit your way
+    console.error(`ERR: ${err}`);
+    process.exit(1);
+  }
 }
+
+main();
